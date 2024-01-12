d9e8421eef7313a907f13f43fdd1c8e6e952fcfa
Renat Chernov <renat3846@gmail.com>
2024-01-12 14:35:13 +0200

    lesson1-hw

--- src/lesson1-homework.ts
+++ src/lesson1-homework.ts
@@ -21,21 +21,45 @@ excercise1();
 // 2. loop which prints string to console n times
 function excercise2() {
   // declare two varaibles - one of string, one of type number
+  let stringValue: string
+  let numberValue: number
   // assign string var value 'Hello'
   // assign number var value 3
+   stringValue = 'Hello';
+   numberValue = 3;
   // create a while loop which prints string variable to console 3 times
+  
+  let count:number = 0;
+  while(count < numberValue) {
+    console.log(stringValue)
+    count++
+  }
   // try to assign number variable to string variable - observe the error
+
+  // stringValue = numberValue; // This line will throw an error -> Type 'number' is not assignable to type 'string'
 }
 excercise2();
 
 // 3. code that generates array of numbers - from n to m
 function excercise3() {
   // TODO: declare varaibles n and m of type number
+  let n:number
+  let m:number
   // TODO: declare varaible result of type array of numbers
+  let result:Array<number> = []
   // TODO: assign n and m some values - n = 1, m = 10
+  n = 1
+  m = 10
   // TODO: create a for loop which adds numbers from n to m to result array
+  for (let i = n; i <= m; i++) {
+    result.push(i);
+  }
   // TODO: support case where m > n (reverse the order)
+  if (m > n) {
+    result.reverse();
+  }
   // TODO: print result array to console
+  console.log(result);
 }
 excercise3();
 // TODO: compile and run the code
