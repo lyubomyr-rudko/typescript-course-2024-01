8ec8bcf81fe30924f23c876c6b1c299295510c50
NikitaDrzhevetskyi <n.drzhevetskyi@gmail.com>
2024-01-13 20:26:19 +0200

    TS-HW-1 is done

--- src/lesson1-homework.ts
+++ src/lesson1-homework.ts
@@ -21,21 +21,45 @@ excercise1();
 // 2. loop which prints string to console n times
 function excercise2() {
   // declare two varaibles - one of string, one of type number
+  let num: number;
+  let str: string;
   // assign string var value 'Hello'
+  str = 'Hello';
   // assign number var value 3
+  num = 3;
   // create a while loop which prints string variable to console 3 times
+  let i = 0;
+  while (i < 3) {
+    console.log(str);
+    i++;
+  }
   // try to assign number variable to string variable - observe the error
+  //str = num //Error
 }
 excercise2();
 
 // 3. code that generates array of numbers - from n to m
 function excercise3() {
   // TODO: declare varaibles n and m of type number
+  let n: number;
+  let m: number;
   // TODO: declare varaible result of type array of numbers
+  let arr: number[] = [];
   // TODO: assign n and m some values - n = 1, m = 10
+  n = 1;
+  m = 10;
   // TODO: create a for loop which adds numbers from n to m to result array
+  for (let i = n; i <= m; i++) {
+    arr.push(i);
+  }
+  console.log(arr);
   // TODO: support case where m > n (reverse the order)
+  let reversedArr: number[] = [];
+  for (let i = m; i >= n; i--) {
+    reversedArr.push(i);
+  }
   // TODO: print result array to console
+  console.log(reversedArr);
 }
 excercise3();
 // TODO: compile and run the code
