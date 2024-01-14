c0c6c1b1beb5ae8e096fdfb38b0219d0005f11f2
Yuliia Hrabko <yuliiayggr@gmail.com>
2024-01-14 15:16:34 +0200

    homework1-done

--- src/lesson1-homework.ts
+++ src/lesson1-homework.ts
@@ -21,21 +21,43 @@ excercise1();
 // 2. loop which prints string to console n times
 function excercise2() {
   // declare two varaibles - one of string, one of type number
+  let varaiblesString: string;
+  let varaiblesNumber: number;
   // assign string var value 'Hello'
+  varaiblesString = 'Hello';
   // assign number var value 3
+  varaiblesNumber = 3;
   // create a while loop which prints string variable to console 3 times
+  let i = 1;
+  while (i <= varaiblesNumber) {
+    console.log(varaiblesString);
+    i++;
+  }
   // try to assign number variable to string variable - observe the error
+  varaiblesString = varaiblesNumber.toString();
 }
 excercise2();
 
 // 3. code that generates array of numbers - from n to m
 function excercise3() {
   // TODO: declare varaibles n and m of type number
+  let n: number;
+  let m: number;
   // TODO: declare varaible result of type array of numbers
+  let result: number[] = [];
   // TODO: assign n and m some values - n = 1, m = 10
+  n = 1;
+  m = 10;
   // TODO: create a for loop which adds numbers from n to m to result array
+  for (let i = n; i <= m; i++) {
+    result.push(i);
+  }
   // TODO: support case where m > n (reverse the order)
+  if (m > n) {
+    result.reverse();
+  }
   // TODO: print result array to console
+  console.log(result);
 }
 excercise3();
 // TODO: compile and run the code
