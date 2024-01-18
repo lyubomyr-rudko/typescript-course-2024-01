5b7083368a6028cab150226e2e24a742d54afaf8
Lyubomyr Rudko <lrudko@shutterstock.com>
2024-01-18 22:11:14 +0200

    lesson 3

--- src/lesson3-homework.ts
+++ src/lesson3-homework.ts
@@ -37,83 +37,23 @@ function excercise11() {
 // TODO: compile and run the code
 excercise11();
 
-// create a generic function which takes an array of items of type T and returns the random item from the array
-function excercise12() {
-  // TODO: create a function that takes an array of numbers and returns a random number from the array
-  // TODO: create a function that takes an array of strings and returns a random string from the array
-  // TODO: create a function that takes an array of objects and returns a random object from the array
-  // TODO: observe the same structure of the functions above, and create a generic function which takes an array of items of type T and returns the random item from the array
-}
-// TODO: compile and run the code
-excercise12();
+// TODO: create a function which takes a string and returns a string with all vowels removed
+export function removeAllVovels() {}
 
-// add type assertion to the code
-function excercise13() {
-  // NOTE: do not change this function
-  function fetchUserAge(): unknown {
-    const responseText = '{"name": "John", "age": 18}';
-    return JSON.parse(responseText).age;
-  }
-  const userAge = fetchUserAge();
-  // TODO: uncomment the following code and add type assertion to fix the error
-  // console.log(userAge + 1);
-}
-// TODO: compile and run the code
-excercise13();
+// TODO: create a function which takes an array of strings and returns the array of strings with all vowels removed
+export function removeVowelsFromArray() {}
 
-// use type casting to fix the mistake in the code
-// run the code before and after adding type casting to see the difference
-function excercise14() {
-  function fetchUserAge() {
-    const responseText = '{"name": "John", "age": "16"}';
+// TODO: create a function that checks if a string is a palindrome
+export // polindrome is a word that is the same when read backwards
+function isPalindromeString() {}
 
-    return JSON.parse(responseText).age;
-  }
-  const userAge = fetchUserAge();
-  // TODO: run the code below and observe the result, explain why it is happening,
-  // TODO: add type casting to the function above, convert the age to number, fix the errors
-  if (userAge === 16) {
-    console.log('Time to get your driver license');
-  } else if (userAge > 16) {
-    console.log('You are old enough to drive');
-  } else {
-    console.log('You are not old enough to drive');
-  }
-}
-// TODO: compile and run the code
-excercise14();
+// TODO: create a function which takes any number of strings and returns array of strings that are polindromes
+export function getPolindropesOnly() {}
 
-// add type safety to the code which uses any
-function excercise15() {
-  // TODO: declare a type for user object, which has a name property of type string
+// TODO: create a function which takes an array of strings and returns the reversed array of reversed strings
+export function reverseArrayOfStrings() {}
 
-  // TODO: fix the fetchUsers function to return an array of users, not any type
-  function fetchUsers() {
-    // TODO: add type safety to the data variable, annotate it with the type of users
-    const data: unknown = JSON.parse(
-      '{"users": [{"name": "John"}, {"name": "Jane"}]}',
-    );
+// TODO: create a function that takes n param, and generates a list of n random kyivstar phone numbers (097XXXXXXX)
+export function generatePhoneNumbers() {}
 
-    // TODO: add check for the data type to contain list of users
-    return data;
-  }
-  // TODO: fix typings of the users variable (currently it is of type any)
-  const users = fetchUsers();
-  // TODO: add type safety to the code to print the names of the users to console
-  // users.forEach((user) => console.log(user.name));
-}
-// TODO: compile and run the code
-excercise15();
-
-// use type declarations to fix the comments in the code
-function excercise16() {
-  // TODO: add code which uses process.env.NODE_ENV variable,
-  // TODO: try to compile and see the error
-  // TODO: add type declaration for process.env.NODE_ENV variable in global.d.ts file
-  // TODO: try to compile and see the error fixed
-  // TODO: remove global.d.ts file, copile and see the error again
-  // TODO: install type declarations from error message -  @types/node
-  // NOTE: For the most part, type declaration packages should always have the same name as the package name on npm, but prefixed with @types/
-}
-// TODO: compile and run the code
-excercise16();
+// TODO: write unit-tests for the six functions above
