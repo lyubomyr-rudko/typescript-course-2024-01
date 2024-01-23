a0c78ab51d63a9913b8fd3f8074b0ee7745bdfbf
Yelyzaveta Berest <lizaberest97@gmail.com>
2024-01-23 23:05:58 +0200

    finished without tests

--- src/lesson4-homework.ts
+++ src/lesson4-homework.ts
@@ -1,9 +1,45 @@
+import validator from 'validator';
+import { add } from '@elzbthbrst/test-npm-package';
 // create a generic function which takes an array of items of type T and returns the random item from the array
 function excercise13A() {
   // TODO: create a function that takes an array of numbers and returns a random number from the array
+
+  //   function getRandomNumber(arr: number[]): number {
+  //     const i: number = Math.floor(Math.random() * arr.length);
+  //     return arr[i];
+  //   }
+
   // TODO: create a function that takes an array of strings and returns a random string from the array
+
+  //   function getRandomString(arr: string[]): string {
+  //     const i: number = Math.floor(Math.random() * arr.length);
+  //     return arr[i];
+  //   }
+
   // TODO: create a function that takes an array of objects and returns a random object from the array
+
+  //   type TUser = {
+  //     name: string;
+  //   };
+  //   function getRandomObject(arr: TUser[]): TUser {
+  //     const i: number = Math.floor(Math.random() * arr.length);
+  //     return arr[i];
+  //   }
+
   // TODO: observe the same structure of the functions above, and create a generic function which takes an array of items of type T and returns the random item from the array
+  function getRandom<T>(arr: T[]): T {
+    const i: number = Math.floor(Math.random() * arr.length);
+    return arr[i];
+  }
+  const answ1 = getRandom([1, 2, 3, 6, 8, 9]);
+  const answ2 = getRandom(['apple', 'banana', 'apricot', 'cucumber']);
+  const answ3 = getRandom([
+    { name: 'Liza' },
+    { name: 'Andrey' },
+    { name: 'Mark' },
+    { name: 'Marina' },
+  ]);
+  console.log(answ1, answ2, answ3);
 }
 // TODO: compile and run the code
 // TODO: write unit-tests for the function above, passing different types of arrays to it
@@ -12,10 +48,22 @@ excercise13A();
 // TODO: create a generic function that takes an array of items, and number of items, and generates a chunked array
 // TODO: for example, if the input array is [1, 2, 3, 4, 5] and the number of items is 2, the output should be [[1, 2], [3, 4], [5]]
 // [1, 2, 3, 4, 5, 6, 7, 8, 9], 3 => [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-function excercise13B() {}
+export function excercise13B<T>(arr: T[], n: number): T[][] {
+  const res: T[][] = [];
+  let smallArr: T[] = [];
+  for (let i: number = 0; i <= arr?.length - 1; i++) {
+    smallArr.push(arr[i]);
+    if (smallArr.length === n || i === arr?.length - 1) {
+      res.push(smallArr);
+      smallArr = [];
+    }
+  }
+  return res;
+}
+
 // TODO: compile and run the code
 // TODO: write unit-tests for the function above, passing different types of arrays to it, and different number of items
-excercise13B();
+console.log(excercise13B([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
 
 // use type assertions to fix the error in the code
 function excercise14() {
@@ -26,8 +74,7 @@ function excercise14() {
   }
   const userAge = fetchUserAge();
   // TODO: uncomment the following code and add type assertion to fix the error
-  // console.log(userAge + 1);
-  console.log(userAge); // TODO remove this line
+  console.log((userAge as number) + 1);
 }
 // TODO: compile and run the code
 excercise14();
@@ -40,11 +87,14 @@ function excercise15() {
 
     return JSON.parse(responseText).age;
   }
-  const userAge = fetchUserAge();
+  const userAge = +fetchUserAge(); // add '+' to make userAge number type
   // TODO: run the code below and observe the result, explain why it is happening,
+  console.log(typeof userAge); //string
+  //You are not old enough to drive -> because type of userAge is string, so first and second if are ignored
+
   // TODO: add type casting to the function above, convert the age to number, fix the errors
   if (userAge === 16) {
-    console.log('Time to get your driver license');
+    console.log('Time to get your driver license'); // when add '+' to const userAge = +fetchUserAge() i have this log
   } else if (userAge > 16) {
     console.log('You are old enough to drive');
   } else {
@@ -57,11 +107,25 @@ excercise15();
 // use type declarations to fix the comments in the code
 function excercise16() {
   // TODO: add code which uses process.env.NODE_ENV variable,
+  console.log(process.env.NODE_ENV); //undefined
+  console.log(validator.isEmail('foo@bar.com'));
   // TODO: try to compile and see the error
+
+  //   src/lesson4-homework.ts:1:23 - error TS7016: Could not find a declaration file for module 'validator'. 'D:/hillel ts/typescript-course-2024-01/node_modules/validator/index.js' implicitly has an 'any' type.
+  //   Try `npm i --save-dev @types/validator` if it exists or add a new declaration (.d.ts) file containing `declare module 'validator';`
+  // 1 import validator from 'validator';
+
   // TODO: add type declaration for process.env.NODE_ENV variable in global.d.ts file
   // TODO: try to compile and see the error fixed
-  // TODO: rename global.d.ts to global.d.ts.disabled file, copile and see the error again
+
+  //no errors
+
+  // TODO: rename global.d.ts to global.d.ts.disabled file, compile and see the error again
+
+  //the same error
+
   // TODO: install type declarations from error message -  @types/node
+  //npm install --save @types/validator
   // NOTE: Remember - most of the times type declaration packages should always have the same name as the package name on npm, but prefixed with @types/
 }
 // TODO: compile and run the code
@@ -84,20 +148,21 @@ function exercise17() {
   //   "build": "tsc",
   //   "start": "tsc -w",
   // },
-  // TODO: update package name to @yourusername/test-npm-package
-  // TODO: make sure yourusername is the same as your npmjs.com username
-  // TODO: create ./index.ts file with the following code
-  // export const add = (a: number, b: number) => a + b;
-  // TODO: run npm run build to compile the code > npm run build
-  // TODO: add your changes and push to git repository
-  // TODO: run publish command > npm publish --access=public
-  // TODO: check your package on npmjs.com
-  // TODO: update your package version > npm version patch
-  // TODO: run publish command > npm publish --access=public
-  // TODO: check your package on npmjs.com and see the updated version
+  // TODO: update package name to @yourusername/test-npm-package+
+  // TODO: make sure yourusername is the same as your npmjs.com username+
+  // TODO: create ./index.ts file with the following code+
+  // export const add = (a: number, b: number) => a + b;+
+  // TODO: run npm run build to compile the code > npm run build+
+  // TODO: add your changes and push to git repository+
+  // TODO: run publish command > npm publish --access=public+
+  // TODO: check your package on npmjs.com+
+  // TODO: update your package version > npm version patch+
+  // TODO: run publish command > npm publish --access=public+
+  // TODO: check your package on npmjs.com and see the updated version+
   // TODO: open/create another js project, install your package > npm install @yourusername/test-npm-package
   // TODO: add import statement to the code > import { add } from '@yourusername/test-npm-package';
   // TODO: use add function call in the code > console.log(add(1, 2));
+  console.log(add(1, 2));
   // TODO: compile and run the code
   // https://www.youtube.com/watch?v=J4b_T-qH3BY - how to publish npm package, in case you are stuck
 }
@@ -105,6 +170,11 @@ exercise17();
 
 // create react app with typescript
 function excercise18() {
-  //
+  //https://github.com/elzbthbrst/amazon-agency i have one made by tutorial
 }
 excercise18();
+
+export function getRandom<T>(arr: T[]): T {
+  const i: number = Math.floor(Math.random() * arr.length);
+  return arr[i];
+}
