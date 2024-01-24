c9767f82b2e6f01c8ec245d6ce50d12a5f97c04c
Renat <renat3846@gmail.com>
2024-01-24 23:54:05 +0200

    homework4

--- src/lesson4-homework.ts
+++ src/lesson4-homework.ts
@@ -1,48 +1,113 @@
-// create a generic function which takes an array of items of type T and returns the random item from the array
+// + create a generic function which takes an array of items of type T and returns the random item from the array
 function excercise13A() {
-  // TODO: create a function that takes an array of numbers and returns a random number from the array
-  // TODO: create a function that takes an array of strings and returns a random string from the array
-  // TODO: create a function that takes an array of objects and returns a random object from the array
-  // TODO: observe the same structure of the functions above, and create a generic function which takes an array of items of type T and returns the random item from the array
+  function getRandomItem<T>(items: T[]): T | undefined {
+    if (items.length === 0) {
+      return undefined;
+    }
+
+    const randomIndex = Math.floor(Math.random() * items.length);
+    return items[randomIndex];
+  }
+
+  // +TODO: create a function that takes an array of numbers and returns a random number from the array
+  function getRandomNumber(numbers: number[]): number | undefined {
+    return getRandomItem(numbers);
+  }
+  // +TODO: create a function that takes an array of strings and returns a random string from the array
+  function getRandomString(strings: string[]): string | undefined {
+    return getRandomItem(strings);
+  }
+  // +TODO: create a function that takes an array of objects and returns a random object from the array
+  function getRandomObject<T>(objects: T[]): T | undefined {
+    return getRandomItem(objects);
+  }
+  // +TODO: observe the same structure of the functions above, and create a generic function which takes an array of items of type T and returns the random item from the array
+  const numbers = [1, 2, 3, 4, 5];
+  const strings = ['apple', 'banana', 'orange'];
+  const objects = [{ name: 'John' }, { name: 'Jane' }, { name: 'Doe' }];
+
+  console.log(getRandomNumber(numbers));
+  console.log(getRandomString(strings));
+  console.log(getRandomObject(objects));
+
+  // +test the generic function with different types of arrays
 }
-// TODO: compile and run the code
-// TODO: write unit-tests for the function above, passing different types of arrays to it
+
+//-------------------------------------------------//
+
 excercise13A();
+// +TODO: compile and run the code
+// +TODO: write unit-tests for the function above, passing different types of arrays to it
+export function getRandomItem<T>(items: T[]): T | undefined {
+  if (items.length === 0) {
+    return undefined;
+  }
+  const randomIndex = Math.floor(Math.random() * items.length);
+  return items[randomIndex];
+}
+export function getRandomNumber(numbers: number[]): number | undefined {
+  return getRandomItem(numbers);
+}
+export function getRandomString(strings: string[]): string | undefined {
+  return getRandomItem(strings);
+}
+export function getRandomObject<T>(objects: T[]): T | undefined {
+  return getRandomItem(objects);
+}
+
+//-------------------------------------------------//
 
 // TODO: create a generic function that takes an array of items, and number of items, and generates a chunked array
 // TODO: for example, if the input array is [1, 2, 3, 4, 5] and the number of items is 2, the output should be [[1, 2], [3, 4], [5]]
 // [1, 2, 3, 4, 5, 6, 7, 8, 9], 3 => [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 function excercise13B() {}
-// TODO: compile and run the code
-// TODO: write unit-tests for the function above, passing different types of arrays to it, and different number of items
+function chunkArray<T>(array: T[], chunkSize: number): T[][] {
+  if (chunkSize <= 0) {
+    throw new Error('Chunk size must be greater than 0');
+  }
+
+  const chunkedArray: T[][] = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunkedArray.push(array.slice(index, (index += chunkSize)));
+  }
+
+  return chunkedArray;
+}
+export default chunkArray; // TODO: write unit-tests for the function above, passing different types of arrays to it, and different number of items
 excercise13B();
 
-// use type assertions to fix the error in the code
+//-------------------------------------------------//
+
+// + use type assertions to fix the error in the code
 function excercise14() {
   // NOTE: do not change this function
   function fetchUserAge(): unknown {
     const responseText = '{"name": "John", "age": 18}';
     return JSON.parse(responseText).age;
   }
-  const userAge = fetchUserAge();
-  // TODO: uncomment the following code and add type assertion to fix the error
-  // console.log(userAge + 1);
-  console.log(userAge); // TODO remove this line
+  const userAge = fetchUserAge() as number;
+  // +TODO: uncomment the following code and add type assertion to fix the error
+  console.log(userAge + 1);
 }
-// TODO: compile and run the code
+// +TODO: compile and run the code
 excercise14();
 
-// use type casting to fix the mistake in the code
-// run the code before and after adding type casting to see the difference
+//-------------------------------------------------//
+
+//+ use type casting to fix the mistake in the code
+//+ run the code before and after adding type casting to see the difference
 function excercise15() {
   function fetchUserAge() {
     const responseText = '{"name": "John", "age": "16"}';
 
     return JSON.parse(responseText).age;
   }
-  const userAge = fetchUserAge();
-  // TODO: run the code below and observe the result, explain why it is happening,
-  // TODO: add type casting to the function above, convert the age to number, fix the errors
+  // +TODO: run the code below and observe the result, explain why it is happening,
+  // +TODO: add type casting to the function above, convert the age to number, fix the errors
+  const userAge = Number(fetchUserAge()); // Type casting to convert userAge to number
+
   if (userAge === 16) {
     console.log('Time to get your driver license');
   } else if (userAge > 16) {
@@ -51,26 +116,30 @@ function excercise15() {
     console.log('You are not old enough to drive');
   }
 }
-// TODO: compile and run the code
+// +TODO: compile and run the code
 excercise15();
+
+//-------------------------------------------------//
 
 // use type declarations to fix the comments in the code
 function excercise16() {
   // TODO: add code which uses process.env.NODE_ENV variable,
+  console.log(process.env.NODE_ENV);
   // TODO: try to compile and see the error
   // TODO: add type declaration for process.env.NODE_ENV variable in global.d.ts file
   // TODO: try to compile and see the error fixed
-  // TODO: rename global.d.ts to global.d.ts.disabled file, copile and see the error again
+  // TODO: rename global.d.ts to global.d.ts.disabled file, compile and see the error again
   // TODO: install type declarations from error message -  @types/node
   // NOTE: Remember - most of the times type declaration packages should always have the same name as the package name on npm, but prefixed with @types/
 }
 // TODO: compile and run the code
 excercise16();
+//-------------------------------------------------//
 
 // create an npm package of your own
 function exercise17() {
-  // TODO: register on npmjs.com
-  // TODO: check email and confirm npm account email
+  // +TODO: register on npmjs.com
+  // +TODO: check email and confirm npm account email
   // TODO: create a git repository for your package, name it test-npm-package
   // TODO: clone the repository to your local machine
   // TODO: run npm login to login to npm > npm login
