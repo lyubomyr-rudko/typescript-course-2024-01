2548146398661801d2a862e89a341ec9cea2370c
Oleksii Fadieiev <ofadieiev@macpaw.com>
2024-01-25 18:55:12 +0100

    Revert "HW3 test"
    This reverts commit bff0bac40b5ce447f592577f39c6bb826bc6f6ab.

--- src/lesson4-homework.ts
+++ src/lesson4-homework.ts
@@ -1,64 +1,24 @@
 // create a generic function which takes an array of items of type T and returns the random item from the array
-function exercise13A() {
+function excercise13A() {
   // TODO: create a function that takes an array of numbers and returns a random number from the array
-  function randomNumberFromArray(arr: number[]): number {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
-  console.log(randomNumberFromArray([1, 5, 2, 9]));
   // TODO: create a function that takes an array of strings and returns a random string from the array
-  function randomStringFromArray(arr: string[]): string {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
-  console.log(randomStringFromArray(['i', 'am', 'groot']));
   // TODO: create a function that takes an array of objects and returns a random object from the array
-  function randomObjectFromArray(arr: { name: string }[]) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
-  console.log(randomObjectFromArray([{ name: 'Tor' }, { name: 'Loki' }]));
   // TODO: observe the same structure of the functions above, and create a generic function which takes an array of items of type T and returns the random item from the array
-  function randomItemFromArray<T>(arr: Array<T>) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
-  console.log(randomItemFromArray([9, 4, 6]));
-  console.log(randomItemFromArray(['Hello', 'world']));
-  console.log(randomItemFromArray([{ god: 1 }, { god: 2 }]));
-  console.log(randomItemFromArray([1, 'home', { name: 'Alex' }]));
 }
 // TODO: compile and run the code
 // TODO: write unit-tests for the function above, passing different types of arrays to it
-export function randomItemFromArray<T>(arr: Array<T>) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
-exercise13A();
+excercise13A();
 
 // TODO: create a generic function that takes an array of items, and number of items, and generates a chunked array
 // TODO: for example, if the input array is [1, 2, 3, 4, 5] and the number of items is 2, the output should be [[1, 2], [3, 4], [5]]
 // [1, 2, 3, 4, 5, 6, 7, 8, 9], 3 => [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-function exercise13B() {
-  function chunk<T>(array: Array<T>, size: number) {
-    const chunkedArray = [];
-    for (let i = 0; i < array.length; i += size) {
-      chunkedArray.push(array.slice(i, i + size));
-    }
-    return chunkedArray;
-  }
-  console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
-}
-export function chunk<T>(array: Array<T>, size: number) {
-  const chunkedArray = [];
-  for (let i = 0; i < array.length; i += size) {
-    chunkedArray.push(array.slice(i, i + size));
-  }
-  return chunkedArray;
-}
-console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9], 8));
+function excercise13B() {}
 // TODO: compile and run the code
 // TODO: write unit-tests for the function above, passing different types of arrays to it, and different number of items
-exercise13B();
+excercise13B();
 
 // use type assertions to fix the error in the code
-function exercise14() {
+function excercise14() {
   // NOTE: do not change this function
   function fetchUserAge(): unknown {
     const responseText = '{"name": "John", "age": 18}';
@@ -66,20 +26,21 @@ function exercise14() {
   }
   const userAge = fetchUserAge();
   // TODO: uncomment the following code and add type assertion to fix the error
-  console.log((userAge as number) + 1);
+  // console.log(userAge + 1);
+  console.log(userAge); // TODO remove this line
 }
 // TODO: compile and run the code
-exercise14();
+excercise14();
 
 // use type casting to fix the mistake in the code
 // run the code before and after adding type casting to see the difference
-function exercise15() {
+function excercise15() {
   function fetchUserAge() {
     const responseText = '{"name": "John", "age": "16"}';
 
     return JSON.parse(responseText).age;
   }
-  const userAge: number = +fetchUserAge();
+  const userAge = fetchUserAge();
   // TODO: run the code below and observe the result, explain why it is happening,
   // TODO: add type casting to the function above, convert the age to number, fix the errors
   if (userAge === 16) {
@@ -91,10 +52,10 @@ function exercise15() {
   }
 }
 // TODO: compile and run the code
-exercise15();
+excercise15();
 
 // use type declarations to fix the comments in the code
-function exercise16() {
+function excercise16() {
   // TODO: add code which uses process.env.NODE_ENV variable,
   // TODO: try to compile and see the error
   // TODO: add type declaration for process.env.NODE_ENV variable in global.d.ts file
@@ -102,11 +63,9 @@ function exercise16() {
   // TODO: rename global.d.ts to global.d.ts.disabled file, copile and see the error again
   // TODO: install type declarations from error message -  @types/node
   // NOTE: Remember - most of the times type declaration packages should always have the same name as the package name on npm, but prefixed with @types/
-  const env = process.env.NODE_ENV;
-  console.log(env);
 }
 // TODO: compile and run the code
-exercise16();
+excercise16();
 
 // create an npm package of your own
 function exercise17() {
