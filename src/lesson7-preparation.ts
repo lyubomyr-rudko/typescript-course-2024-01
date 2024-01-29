65084f8cb58155942bb7d36451e45dc716823833
Lyubomyr Rudko <lrudko@shutterstock.com>
2024-01-29 21:56:41 +0200

    lesson 6

---
+++ src/lesson7-preparation.ts
@@ -0,0 +1,7 @@
+// ********* Lesson 7 *********
+
+// Q/A
+// React with Typescript - class components, functional components, hooks, jest dependency mocks
+
+const test = 'test';
+export default test;
