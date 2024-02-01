97886297bcce9a46c06f8652a1a7b8f27dd6f790
Yuliia Hrabko <yuliiayggr@gmail.com>
2024-02-01 19:19:27 +0200

    homework6-yuliia-hrabko without test

--- src/lesson6-homework.ts
+++ src/lesson6-homework.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// import { first } from 'lodash';
 
 const test = 'test'; // this is mock export
 export default test;
@@ -9,36 +10,75 @@ function exercise27() {
   // TODO: define THuman type with properties name, age, driverLicenseId
   type THuman = {
     name: string;
+    age: number;
+    driverLicenseId: string;
   };
   // TODO: define TAnimal type with properties name, age, species
   type TAnimal = {
     name: string;
+    age: number;
+    species: string;
   };
   // TODO: define TPassanger type as union of THuman and TAnimal
-  // type TPassanger = {};
+  type TPassanger = THuman | TAnimal;
 
   // annotate the function to accept TPassanger type
-  function printPassangerInfo(passanger: unknown) {
+  function printPassangerInfo(passanger: TPassanger) {
     // TODO: use type narrowing to print the passanger info
-    // console.log((passanger as any).name);
-    // console.log((passanger as any).age);
+    console.log(passanger.name);
+    console.log(passanger.age);
     // TODO: print driverLicenseId if passanger is human
-    // console.log((passanger as any).driverLicenseId);
+    if ('driverLicenseId' in passanger) {
+      console.log(passanger.driverLicenseId);
+    }
     // TODO: print species if passanger is animal
-    // console.log((passanger as any).species);
+    if ('species' in passanger) {
+      console.log(passanger.species);
+    }
     console.log(passanger);
   }
   // TODO: add missing properties to human and animal objects
   const human: THuman = {
     name: 'John',
+    age: 25,
+    driverLicenseId: 'dreverLicenseId',
   };
   const animal: TAnimal = {
     name: 'Rex',
+    age: 5,
+    species: 'bulldog',
   };
   printPassangerInfo(human);
   printPassangerInfo(animal);
   // TODO: Implement function printPassangerInfo using instanceof operator to narrow the type of the passanger
   // TODO: Add implementation of the printPassangerInfo using property check to narrow the type of the passanger
+  class Human {
+    constructor(
+      public name: string,
+      public age: number,
+      public driverLicenseId: string,
+    ) {}
+  }
+  class Animal {
+    constructor(
+      public name: string,
+      public age: number,
+      public species: string,
+    ) {}
+  }
+  type TPassanger2 = Human | Animal;
+  function printPassangerInfoWithInstanceOf(passanger: TPassanger2) {
+    if (passanger instanceof Human) {
+      console.log(passanger.driverLicenseId);
+    }
+    if (passanger instanceof Animal) {
+      console.log(passanger.species);
+    }
+  }
+  const human1 = new Human('Juli', 29, 'id');
+  const animal1 = new Animal('Sharik', 2, 'bulldog');
+  printPassangerInfoWithInstanceOf(human1);
+  printPassangerInfoWithInstanceOf(animal1);
 }
 // TODO: compile and run the code
 exercise27();
@@ -47,12 +87,15 @@ exercise27();
 function exercise28() {
   // TODO: add type property to TBlogMessage, TBlogImage, TBlogComment with literal type of 'message', 'image', 'comment'
   type TBlogMessage = {
+    kind: 'message';
     text: string;
   };
   type TBlogImage = {
+    kind: 'image';
     url: string;
   };
   type TBlogComment = {
+    kind: 'comment';
     text: string;
     messageId: string;
   };
@@ -61,21 +104,38 @@ function exercise28() {
 
   function printBlogPost(post: TBlogPost) {
     // TODO: use discriminated union instead of prop check to narrow the type of the object
-    if ('messageId' in post) {
-      console.log('comment: ', post.text);
+    if (post.kind === 'comment') {
+      console.log('comment: ', post.text, post.messageId);
     }
-    if ('url' in post) {
+    if (post.kind === 'image') {
       console.log('image: ', post.url);
     }
-    if ('text' in post) {
+    if (post.kind === 'message') {
       console.log('message: ', post.text);
     }
   }
 
+  const comment: TBlogComment = {
+    kind: 'comment',
+    text: 'abc',
+    messageId: '123',
+  };
+  const image: TBlogImage = {
+    kind: 'image',
+    url: 'abc',
+  };
+  const message: TBlogMessage = {
+    kind: 'message',
+    text: 'abc',
+  };
   // TODO: add missing type property to the objects
-  printBlogPost({ text: 'abc' });
-  printBlogPost({ url: 'abc' });
-  printBlogPost({ text: 'abc', messageId: '123' });
+  printBlogPost(message);
+  printBlogPost(image);
+  printBlogPost(comment);
+  //or
+  printBlogPost({ kind: 'image', url: 'abc' });
+  printBlogPost({ kind: 'comment', text: 'abc', messageId: 'abc' });
+  printBlogPost({ kind: 'message', text: 'abc' });
 }
 // TODO: compile and run the code
 exercise28();
@@ -88,7 +148,14 @@ function excerciseA() {
 
   interface IMyComponentProps {
     optionalBool?: boolean;
+    optionalArray?: Message[];
+    optionalFunc?: (id: number) => void;
+    optionalNumber?: number;
+    optionalObject?: { id: number; title: string };
+    optionalString?: string;
+    optionalSymbol?: symbol;
   }
+
   class MyComponent extends React.Component<IMyComponentProps> {
     render() {
       return React.createElement('div', null, 'hello');
@@ -138,12 +205,18 @@ excerciseA();
 
 async function excerciseB() {
   // TODO: define IUser interface with properties id, name, email, website
+  interface IUser {
+    id: number;
+    name: string;
+    email: string;
+    website: string;
+  }
 
   // TODO: implement function to fetch list of users from https://jsonplaceholder.typicode.com/users
-  async function fetchUsers() {
+  async function fetchUsers(): Promise<IUser[]> {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     // TODO: apply type to the result of this fetch function
-    const users = await res.json();
+    const users: IUser[] = await res.json();
 
     return users;
   }
@@ -151,25 +224,85 @@ async function excerciseB() {
   const users = await fetchUsers();
 
   // TODO: extend interface IUser with property address of type IAddress
+  interface IUser {
+    address: IAddress;
+  }
   // TODO: define IAddress interface with properties street, suite, city, zipcode, geo
-  // TODO: extend interface IUser with property company of type ICompany and define ICompany interface with properties name, catchPhrase, bs
+  interface IAddress {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: object[];
+  }
 
+  // TODO: extend interface IUser with property company of type ICompany and define ICompany interface with properties name, catchPhrase, bs
+  interface IUser {
+    company: ICompany;
+  }
+  interface ICompany {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  }
   // TODO: define function that returns array of user names in format { firstName: string, lastName: string}
+  interface IGetUserNames {
+    firstName: string;
+    lastName: string;
+  }
+
   // TODO: use interface type for the function parameter
-  function getUserNames(users: unknown) {
-    console.log(users);
-    return [];
+  function getUserNames(users: IUser[]) {
+    const getUserName = users.map((el) => el.name.split(' '));
+    const getUserFirstLastName: IGetUserNames[] = getUserName.map((el) => {
+      if (el.includes('Mrs.')) {
+        return {
+          firstName: ` ${el[0]} ${el[1]}`,
+          lastName: el.slice(2).join(' '),
+        };
+      } else {
+        return {
+          firstName: el[0],
+          lastName: el.slice(1).join(' '),
+        };
+      }
+    });
+    return getUserFirstLastName;
   }
   console.log(getUserNames(users));
 
   // TODO: define a function that returns array of company names
+  function getCompanyNames(users: IUser[]) {
+    const getCompanyName = users.map(({ company }) => company.name);
+
+    return getCompanyName;
+  }
+  console.log(getCompanyNames(users));
 
   // TODO: define a function that returns a company name that has the longest catchPhrase
+  function getLongerCatchPhrase(users: IUser[]) {
+    const getLongetPhrase = users.map(({ company }) => company.catchPhrase);
+    getLongetPhrase.sort((a, b) => b.length - a.length);
+
+    return getLongetPhrase[0];
+  }
+  console.log(getLongerCatchPhrase(users));
 
   // TODO: define a function that returns a list of users that have website ending with .org
+  function getWebsiteEndinOrg(users: IUser[]) {
+    const getWebsiteEndingWithOrg = users.filter(({ website }) =>
+      website.includes('.org'),
+    );
+    return getWebsiteEndingWithOrg;
+  }
+
+  console.log(getWebsiteEndinOrg(users));
 
   // TODO: define a funciton that returns a list of cities where users live, sorted by city name
-
+  function getListCities(users: IUser[]) {
+    return users.map(({ address }) => address.city).sort();
+  }
+  console.log(getListCities(users));
   // TODO: move all the functions above out of this function and export them
   // TODO: write unit tests for the 4 functions above
 }
