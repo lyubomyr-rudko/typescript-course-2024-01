97fe69e1ddad6302fe8a07347e4e6a2000aa5bec
ArtemPustoutov <artem.pus89@gmail.com>
2024-02-07 21:54:55 +0200

    hw8

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,65 +1,34 @@
 import { describe, expect, it } from '@jest/globals';
-import { sum, max, getRandomNumber } from './index';
-import { distance } from './lesson2-homework';
+import { removeDuplicates, getIntersection } from './lesson8-homework';
 
-describe('sum', () => {
+describe('removeDuplicates', () => {
   it('should be defined', () => {
-    expect(sum).toBeDefined();
+    expect(removeDuplicates).toBeDefined();
   });
 
-  it('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+  it('returns [1, 2, 3, 4, 5]', () => {
+    expect(removeDuplicates([1, 2, 3, 4, 5, 1, 2, 3, 4, 5])).toStrictEqual([
+      1, 2, 3, 4, 5,
+    ]);
   });
 
-  it('adds 0 + 0 to equal 0', () => {
-    expect(sum(0, 0)).toBe(0);
+  it('must contain 4', () => {
+    expect(removeDuplicates([1, 2, 3, 4, 5, 1, 2, 3, 4, 5])).toContain(4);
   });
 });
 
-describe('max', () => {
+describe('getIntersection', () => {
   it('should be defined', () => {
-    expect(max).toBeDefined();
+    expect(getIntersection).toBeDefined();
   });
 
-  it('returns 3 for 1, 2, 3', () => {
-    expect(max(1, 2, 3)).toBe(3);
+  it('returns [3, 4]', () => {
+    expect(getIntersection([8, 3, 2, 4, 2], [7, 3, 4, 5, 3])).toStrictEqual([
+      3, 4,
+    ]);
   });
 
-  it('returns 0 for 0, 0, 0, 0', () => {
-    expect(max(0, 0, 0, 0)).toBe(0);
-  });
-});
-
-describe('distance', () => {
-  it('should be defined', () => {
-    expect(distance).toBeDefined();
-  });
-});
-
-describe('getRandomNumber', () => {
-  beforeAll(() => {
-    const mock = jest
-      .fn()
-      .mockReturnValueOnce(0.5)
-      .mockReturnValueOnce(0.2)
-      .mockReturnValueOnce(0.8);
-
-    // for (let i = 0; i < 10; i++) {
-    //   mock.mockReturnValueOnce(0.5);
-    //   mock.mockReturnValueOnce(0.2);
-    //   mock.mockReturnValueOnce(0.8);
-    // }
-
-    Math.random = mock;
-  });
-
-  afterAll(() => {
-    jest.resetAllMocks();
-  });
-
-  it('should return random number', () => {
-    expect(getRandomNumber()).toBe(5);
-    expect(getRandomNumber()).toBe(2);
-    expect(getRandomNumber()).toBe(8);
+  it('must contain 4', () => {
+    expect(removeDuplicates([1, 2, 3, 4, 5, 1, 2, 3, 4, 5])).toContain(4);
   });
 });
