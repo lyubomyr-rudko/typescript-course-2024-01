096687df1afe0d6051e2b477a42d5a9b238a8ca1
Stanislav Brovin <stb@sekai.io>
2024-02-12 11:06:57 +0200

    First part HW - exercise40

--- src/lesson9-homework.ts
+++ src/lesson9-homework.ts
@@ -1,201 +1,169 @@
-// // Read the following typescript documentation (if you haven't already)
-// // Use Translate to Ukrainian
-// // 1. https://www.typescriptlang.org/docs/handbook/2/everyday-types.html
-// // 2. https://www.typescriptlang.org/docs/handbook/2/narrowing.html
-// // 3. https://www.typescriptlang.org/docs/handbook/2/functions.html
+// Read the following typescript documentation (if you haven't already)
+// Use Translate to Ukrainian
+// 1. https://www.typescriptlang.org/docs/handbook/2/everyday-types.html
+// 2. https://www.typescriptlang.org/docs/handbook/2/narrowing.html
+// 3. https://www.typescriptlang.org/docs/handbook/2/functions.html
 
-// // Read the following typescript documentation on decorators:
-// // Use Translate to Ukrainian
-// // 1. https://www.typescriptlang.org/docs/handbook/decorators.html#handbook-content
-// // 2. https://mirone.me/a-complete-guide-to-typescript-decorator/
-// // 3. https://devblogs.microsoft.com/typescript/announcing-typescript-5-0/#decorators
+// Read the following typescript documentation on decorators:
+// Use Translate to Ukrainian
+// 1. https://www.typescriptlang.org/docs/handbook/decorators.html#handbook-content
+// 2. https://mirone.me/a-complete-guide-to-typescript-decorator/
+// 3. https://devblogs.microsoft.com/typescript/announcing-typescript-5-0/#decorators
 
-// // Use experimental decorators
-// function exercise40() {
-//   // TODO: 1. implement method decorator to print call count of the function
-//   // function logCountCalls(
-//   //   target: any,
-//   //   propertyKey: string,
-//   //   descriptor: PropertyDescriptor,
-//   // ): any {
-//   //   // console.log(JSON.stringify({ target, propertyKey, descriptor }));
-//   //   const originalFn = descriptor.value;
+// Use experimental decorators
+/* eslint-disable @typescript-eslint/no-explicit-any */
+function exercise40() {
+  // for used static  methods
+  // function LogCauntCallsFunc(
+  //   target: any,
+  //   key: string,
+  //   descriptor: PropertyDescriptor,
+  // ) {
+  //   let callCount = 0;
 
-//   //   return function (this: any, ...args: any[]) {
-//   //     let calls: number = 0;
-//   //     const result = originalFn.call(this, ...args);
-//   //     console.log(`Method called ${calls} times.`);
-//   //     return result;
-//   //   };
-//   // }
+  //   const originalMethod = descriptor.value;
 
-//   // function logCountCalls(originalFn: any, ...args: any[]): any {
-//   //   let calls: number = 0;
-//   //   return function (this: any, ...args: any[]) {
-//   //     const result = originalFn.call(this, ...args);
-//   //     console.log(`Method called ${calls} times.`);
-//   //     return result;
-//   //   };
-//   // }
+  //   descriptor.value = function (...args: any[]) {
+  //     callCount++;
+  //     console.log(`Function ${key} has been called ${callCount} time(s).`);
+  //     return originalMethod.apply(this, args);
+  //   };
 
-//   function logCountCalls(
-//     target: any,
-//     propertyKey: string,
-//     descriptor: PropertyDescriptor,
-//   ) {
-//     console.log('target: ', target);
-//     console.log('propertyKey: ', propertyKey);
-//     console.log('descriptor: ', descriptor);
+  //   return descriptor;
+  // }
 
-//     // work code from lecture BUT can compiled in TS:  - Property 'calls' does not exist on type '() => void'  any
-//     // function printCallCount() {
-//     //   if (!printCallCount.calls) printCallCount.calls = 1;
-//     //   else printCallCount.calls++;
-//     //   console.log('Calls: ', printCallCount.calls);
-//     // }
-//     // printCallCount();
+  interface IWrapperStatic {
+    add(a: number, b: number): number;
+    log(a: number, b: number): void;
+    timer(a: number, b: number): void;
+  }
 
-//     // my version
-//     // const originalFn = descriptor.value;
-//     let calls: number = 0;
-//     function logger(this: any, ...args: any[]): any {
-//       calls++;
-//       console.log(`Method ${propertyKey} called ${calls} times.`);
-//       const res = target.call(this, ...args);
-//       return res;
-//       // return descriptor.value.call(this, args);
-//     }
-//     return logger;
-//   }
+  // function LogExecuteTimeFunc (...args: any[]){
+  //   console.log(args);
+  // }
 
-//   // TODO: 2. implement method decorator to print execution time of the function
-//   function printExceutionTime(originalFn: any, ...args: any[]): any {
-//     return function (this: any, ...args: any[]) {
-//       let startTime = Date.now();
-//       let res = originalFn.apply(this, args);
-//       let endTime = Date.now();
-//       console.log(`Executed time: ${endTime - startTime} ms.`);
-//       return res;
-//     };
-//   }
-//   // function printExceutionTime() {
-//   //   return (
-//   //     target: any,
-//   //     propertyKey: string,
-//   //     descriptor: PropertyDescriptor,
-//   //   ) => {
-//   //     const originalFn = descriptor.value;
+  // BAD IDEA FOR TS :
+  // In your case, you're trying to use a decorator on a function declaration, but
+  //  decorators in TypeScript are only allowed on classes, methods, accessor declarations, properties, or parameters.
+  // @LogExecuteTimeFunc
+  function WrapperStatic(constructor: any) {
+    // const add = function (a: number, b: number) {
+    //   console.log('wrapper work ', a, b);
+    //   return a + b;
+    // };
+    // constructor.prototype.add = LogCauntCallsFunc(this, 'add', { value: add });
+    let counter = 0;
+    constructor.prototype.add = function (a: number, b: number) {
+      console.log('wrapper work ', a, b);
+      return a + b;
+    };
+    constructor.prototype.log = function (a: number, b: number) {
+      counter++;
+      this.add(a, b);
+      console.log(`Called: ${counter} times for ${a}+${b}`);
+    };
+    constructor.prototype.timer = function (a: number, b: number) {
+      const start = Date.now();
+      setTimeout(
+        async () => {
+          this.add(a, b);
+          const end = Date.now();
+          console.log(
+            `For ${a}+${b} Start: ${start}, End: ${end}, Executed time: ${
+              end - start
+            }`,
+          );
+        },
+        Math.round(Math.random() * 10),
+      );
+    };
+  }
 
-//   //     descriptor.value = function (...args: any[]) {
-//   //       let startTime = Date.now();
-//   //       let result = originalFn.apply(this, args);
-//   //       let endTime = Date.now();
-//   //       console.log(
-//   //         `Method ${propertyKey} executed in ${endTime - startTime} ms.`,
-//   //       );
-//   //       return result;
-//   //     };
+  // TODO: 1. implement method decorator to print call count of the function
+  function logCauntCalls(
+    target: any,
+    methodName: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    console.log('Use method: ', methodName);
+    const origMethod = descriptor.value;
+    let counter = 0;
+    descriptor.value = function (...args: any[]) {
+      counter++;
+      console.log(`${methodName} called: ${counter} times`);
+      return origMethod.apply(this, args);
+    };
+    return descriptor;
+  }
 
-//   //     return descriptor;
-//   //   };
-//   // }
+  // TODO: 2. implement method decorator to print execution time of the function
+  function logExecuteTime(
+    target: any,
+    methodName: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const origMethod = descriptor.value;
 
-//   class Calculation {
-//     // TODO: add both decorators to the following method
-//     @logCountCalls
-//     @printExceutionTime
-//     // bad idea use sctatic for decorator - its won't work for instances
-//     static add1(a: number, b: number): number {
-//       return a + b;
-//     }
-//     // my code for compare work with static idea method
-//     add2(a: number, b: number): number {
-//       return a + b;
-//     }
-//   }
-//   // TODO: create instance of Calculation class and call add method
-//   const calc1 = new Calculation();
-//   // static method can't calls from instances BUT work from root class
-//   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/static?retiredLocale=uk
-//   // https://www.w3schools.com/js/js_class_static.asp
-//   (calc1.constructor as typeof Calculation).add1(1, 1); // SOLVED
-//   (calc1.constructor as typeof Calculation).add1(1, 2);
-//   calc1.add2(2, 2); // good work without blackmagic as must work !
-//   calc1.add2(2, 3);
+    descriptor.value = function (...args: any[]) {
+      const start = Date.now();
+      const res = setTimeout(
+        async () => {
+          const res = await origMethod.apply(this, args);
+          const end = Date.now();
+          console.log(
+            `Method: ${methodName} with params: ${JSON.stringify(
+              args,
+            )}\n\tStart: ${start}, End: ${end}, Executed time: ${end - start}`,
+          );
+          return res;
+        },
+        Math.round(Math.random() * 10),
+      );
 
-//   // TODO: remove the following line
-//   // console.log(Calculation);
-// }
-// exercise40();
+      return res;
+    };
+    return descriptor;
+  }
 
-// // Use 2023 decorators (Stage 3 decorator)
-// function exercise41() {
-//   // TODO: 1. implement method decorator to print call count of the function :
-//   function logCountCalls() {
-//     let calls: number = 0;
-//     return (
-//       target: any,
-//       propertyKey: string,
-//       descriptor: PropertyDescriptor,
-//     ) => {
-//       calls++;
-//       console.log('Count calls: ', calls);
-//     };
-//   }
+  @WrapperStatic
+  class Calculation {
+    // TODO: add both decorators to the following method
+    @logCauntCalls
+    @logExecuteTime
+    static add(a: number, b: number) {
+      return a + b;
+    }
+  }
+  type TCalculation = Calculation & IWrapperStatic;
+  // TODO: create instance of Calculation class and call add method
+  const calc = new Calculation() as TCalculation;
+  // static method can't directly calls from instances - only from obj Class OR
+  (calc.constructor as typeof Calculation).add(1, 1);
+  Calculation.add(2, 1);
+  calc.add(2, 2); // decorator can't work for function() on TS - only in JS
+  calc.log(2, 2); // decorator can't work for function() on TS - only in JS
+  calc.timer(2, 2); // decorator can't work for function() on TS - only in JS
 
-//   // TODO: 2. implement method decorator to print execution time of the function
-//   function printExceutionTime() {
-//     return (
-//       target: any,
-//       propertyKey: string,
-//       descriptor: PropertyDescriptor,
-//     ) => {
-//       const originalFn = descriptor.value;
+  // TODO: remove the following line
+  // console.log(Calculation);
+}
+exercise40();
 
-//       descriptor.value = function (...args: any[]) {
-//         if (this instanceof target.constructor) {
-//           const start = +Date.now();
-//           const res = originalFn.apply(target, args);
-//           const end = +Date.now();
-//           console.log(
-//             `Start on: ${start}, End on: ${end}. Execute time: ${end - start}`,
-//           );
-//           // return (constructor.prototype[originalFn] = res);
-//           return res;
-//         } else {
-//           const start = +Date.now();
-//           const res = originalFn.apply(this, args);
-//           const end = +Date.now();
-//           console.log(
-//             `Start on: ${start}, End on: ${end}. Execute time: ${end - start}`,
-//           );
-//           // return (constructor.prototype[originalFn] = res);
-//           return res;
-//         }
-//       };
-//       return descriptor;
-//     };
-//   }
+// Use 2023 decorators (Stage 3 decorator)
+function exercise41() {
+  // TODO: 1. implement method decorator to print call count of the function
 
-//   class Calculation {
-//     // TODO: add both decorators to the following method
-//     @logCountCalls()
-//     @printExceutionTime()
-//     static add(a: number, b: number) {
-//       return a + b;
-//     }
-//   }
-//   // TODO: create instance of Calculation class and call add method
-//   const calc = new Calculation();
-//   // static method can't calls from instances BUT work from root class
-//   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/static?retiredLocale=uk
-//   // https://www.w3schools.com/js/js_class_static.asp
-//   // as sample Calculation.add(3,4) will good work BUT
-//   console.log(
-//     '\nStage 3 decorators sample:\n',
-//     (calc.constructor as typeof Calculation).add(3, 4),
-//   );
-//   // TODO: remove the following line
-//   // console.log(Calculation);
-// }
-// exercise41();
+  // TODO: 2. implement method decorator to print execution time of the function
+
+  class Calculation {
+    // TODO: add both decorators to the following method
+    static add(a: number, b: number) {
+      return a + b;
+    }
+  }
+  // TODO: create instance of Calculation class and call add method
+
+  // TODO: remove the following line
+  console.log(Calculation);
+}
+exercise41();
