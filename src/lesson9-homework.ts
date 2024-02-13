4ed4e7a3706e5dfe67051db5027ec8036e3965e7
Yuliia Hrabko <yuliiayggr@gmail.com>
2024-02-14 00:28:22 +0200

    fixed-homework9

--- src/lesson9-homework.ts
+++ src/lesson9-homework.ts
@@ -13,94 +13,105 @@
 // 3. https://devblogs.microsoft.com/typescript/announcing-typescript-5-0/#decorators
 
 // Use experimental decorators
-// function exercise40() {
-//   // TODO: 1. implement method decorator to print call count of the function
-//   type TDecoration = {
-//     name: string;
-//   };
-//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   function callCount(originalMethod: any, _context: TDecoration) {
-//     let calls: number = 0;
-//     function replacementMethod(this: unknown, ...args: number[]) {
-//       calls++;
-//       const result = originalMethod.call(this, ...args);
+function exercise40() {
+  // TODO: 1. implement method decorator to print call count of the function
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // function callCount(
+  //   target: object,
+  //   key: string,
+  //   descriptor: PropertyDescriptor,
+  // ) {
+  //   let calls: number = 0;
+  //   const originalMethod = descriptor.value;
 
-//       console.log(`${_context.name} = call count ${calls}`);
-//       return result;
-//     }
+  //   descriptor.value = function replacementMethod(
+  //     this: unknown,
+  //     ...args: number[]
+  //   ) {
+  //     calls++;
+  //     const result = originalMethod.call(this, ...args);
 
-//     return replacementMethod;
-//   }
+  //     console.log(`${key} = call count ${calls}`);
+  //     return result;
+  //   };
 
-//   // TODO: 2. implement method decorator to print execution time of the function
-//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   function timeCount(originalMethod: any, _context: TDecoration) {
-//     function replacementMethod(this: unknown, ...args: number[]) {
-//       const timeBefore = new Date().getTime();
-//       const timeAfter = new Date().getTime();
-//       const result = originalMethod.call(this, ...args);
-//       console.log(
-//         `${_context.name} = execution time ${timeAfter - timeBefore}`,
-//       );
-//       return result;
-//     }
+  //   return descriptor;
+  // }
 
-//     return replacementMethod;
-//   }
+  // TODO: 2. implement method decorator to print execution time of the function
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // function timeCount(
+  //   target: object,
+  //   key: string,
+  //   descriptor: PropertyDescriptor,
+  // ) {
+  //   const originalMethod = descriptor.value;
 
-//   class Calculation {
-//     // TODO: add both decorators to the following method
-//     @callCount
-//     @timeCount
-//     static add(a: number, b: number) {
-//       return a + b;
-//     }
-//   }
-//   // TODO: create instance of Calculation class and call add method
+  //   descriptor.value = function replacementMethod(
+  //     this: unknown,
+  //     ...args: number[]
+  //   ) {
+  //     const timeBefore = new Date().getTime();
+  //     const timeAfter = new Date().getTime();
+  //     const result = originalMethod.call(this, ...args);
+  //     console.log(`${key} = execution time ${timeAfter - timeBefore}`);
+  //     return result;
+  //   };
 
-//   // TODO: remove the following line
-//   // console.log(Calculation);
-//   console.log(Calculation.add(1, 2));
-//   console.log(Calculation.add(3000, 4));
-//   console.log(Calculation.add(5, 6));
-// }
-// exercise40();
+  //   return descriptor;
+  // }
+
+  class Calculation {
+    // TODO: add both decorators to the following method
+    // @callCount
+    // @timeCount
+    static add(a: number, b: number) {
+      return a + b;
+    }
+  }
+  // TODO: create instance of Calculation class and call add method
+
+  // TODO: remove the following line
+  // console.log(Calculation);
+  console.log(Calculation.add(1, 2));
+  console.log(Calculation.add(3000, 4));
+  console.log(Calculation.add(5, 6));
+}
+exercise40();
 
 // Use 2023 decorators (Stage 3 decorator)
 function exercise41() {
   // TODO: 1. implement method decorator to print call count of the function
-
+  type TDecoration = {
+    name: string;
+  };
   function callCount(
-    target: object,
-    key: string,
-    descriptor: PropertyDescriptor,
+    originalMethod: (...args: number[]) => number,
+    _context: TDecoration,
   ) {
     let count = 0;
-    const originalMethod = descriptor.value;
-
-    descriptor.value = function (...args: number[]) {
+    function replacementMethod(this: unknown, ...args: number[]) {
       count++;
-      console.log(` ${key}= call count  ${count}`);
-      return originalMethod.apply(this, args);
-    };
+      const result = originalMethod.call(this, ...args);
+      console.log(` ${_context.name}= call count  ${count}`);
+      return result;
+    }
 
-    return descriptor;
+    return replacementMethod;
   }
   // TODO: 2. implement method decorator to print execution time of the function
 
   function executionTime(
-    target: object,
-    key: string,
-    descriptor: PropertyDescriptor,
+    originalMethod: (...args: number[]) => number,
+    _context: TDecoration,
   ) {
-    const originalMethod = descriptor.value;
-    descriptor.value = function (...args: number[]) {
-      console.time(key);
+    function replacementMethod(this: Calculation, ...args: number[]) {
+      console.time(_context.name);
       const result = originalMethod.apply(this, args);
-      console.timeEnd(key);
+      console.timeEnd(_context.name);
       return result;
-    };
-    return descriptor;
+    }
+    return replacementMethod;
   }
   class Calculation {
     @callCount
