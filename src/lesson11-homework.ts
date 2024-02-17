d66164ef070d8542fa37b31b413d1e5c7701bbc1
Yelyzaveta Berest <lizaberest97@gmail.com>
2024-02-17 17:10:42 +0200

    finished hw-11

--- src/lesson11-homework.ts
+++ src/lesson11-homework.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Read the following typescript documentation:
 // 1. https://www.typescriptlang.org/docs/handbook/2/objects.html
 // 2. https://www.typescriptlang.org/docs/handbook/2/generics.html
@@ -6,7 +7,6 @@
 // Use infer keyword
 function exercise46() {
   // TODO: remove the next line
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   function createUser(firstName: string, lastName: string, age: number) {
     const id = (Math.random() * 100000).toString();
 
@@ -19,14 +19,14 @@ function exercise46() {
   }
 
   // TODO: create a type that extracts the type of the first argument of a function FirstParamType
-  // type FirstParameter<T> = ... // use infer keyword
+  type FirstParameter<T> = T extends (...args: infer R) => any ? R[0] : never; // use infer keyword
   // TODO: uncomment the following line and fix the error
-  // type TCreateUserFirstArg = FirstParameter<typeof createUser>; // string
+  type TCreateUserFirstArg = FirstParameter<typeof createUser>; // string
 
   // TODO: create a type that extracts the return type of a function ReturnType
-  // type MyReturnType<T> = ... // use infer keyword
+  type MyReturnType<T> = T extends (...args: any) => infer R ? R : never; // use infer keyword
   // TODO: uncomment the following line and fix the error
-  // type TCreateUserReturnType = MyReturnType<typeof createUser>; // { firstName: string; lastName: string; age: number; id: string; }
+  type TCreateUserReturnType = MyReturnType<typeof createUser>; // { firstName: string; lastName: string; age: number; id: string; }
 
   // TODO: create a type that is a mixture of prev two functions
   // 1. it extracts the type of the first argument of a function FirstParamType
@@ -36,12 +36,9 @@ function exercise46() {
   // type TCreateUserFirstArgAndReturnType = FirstParameterFunction<typeof createUser>; // (firstName: string, lastName: string, age: number) => { firstName: string; lastName: string; age: number; id: string; }
 
   // TODO: replace the next line with the correct type definition
-  type TCreateUserFirstArgAndReturnType = (firstName: string) => {
-    firstName: string;
-    lastName: string;
-    age: number;
-    id: string;
-  };
+  type TCreateUserFirstArgAndReturnType = (
+    arg0: TCreateUserFirstArg,
+  ) => TCreateUserReturnType;
   const fn: TCreateUserFirstArgAndReturnType = (firstName: string) => {
     return {
       firstName,
@@ -54,31 +51,32 @@ function exercise46() {
 }
 exercise46();
 
-// Use mappping types
+// Use mapping types
 function exercise47() {
   // implement TRecord<K,T> mapped type that takes two types T and K
   // K must be a union of strings or numbers or symbols
   // the mapped type should create a new type that has all properties included in list K,
   // and the value of each property is T
-  // type TRecord<K extends ..., T> = {
-  //   [... in ...]: ...;
-  // };
+  type TRecord<K extends string | number | symbol, T> = {
+    [R in K]: T;
+  };
   // TODO: uncomment the following code and check if your mapped type works
-  // type TPoint = TRecord<"x" | "y" | "z", number>;
-  // const point: TPoint = {
-  //   x: 1,
-  //   y: 2,
-  //   z: 3,
-  // };
+  type TPoint = TRecord<'x' | 'y' | 'z', number>;
+  const point: TPoint = {
+    x: 1,
+    y: 2,
+    z: 3,
+  };
+  console.log(point);
 }
 exercise47();
 
-// Use mappping types modifiers
+// Use mapping types modifiers
 function exercise48() {
   // implement mapped type that makes all properties of T optional and nullable
-  // type TPartialNullable<T> = {
-  //   [... in ...]: ...;
-  // };
+  type TPartialNullable<T> = {
+    [K in keyof T]+?: T[K] | null;
+  };
 
   type TPoint = {
     x: number;
@@ -90,9 +88,11 @@ function exercise48() {
   console.log(p0);
 
   // TODO: uncomment the following code and check if your mapped type works
-  // type TNullablePoint = TPartialNullable<TPoint>;
-  // const p1: TNullablePoint = { x: 10 };
-  // const p2: TNullablePoint = { x: 10, y: null };
+  type TNullablePoint = TPartialNullable<TPoint>;
+  const p1: TNullablePoint = { x: 10 };
+  const p2: TNullablePoint = { x: 10, y: null };
+  console.log(p1);
+  console.log(p2);
 }
 exercise48();
 
@@ -103,13 +103,14 @@ function exercise49() {
   // TODO: create a type that represents a string that contains Tshirts sizes and colors (e.g. "S-red", "M-green", "L-blue")
   // TODO: create a function that takes a size and a color and returns a Tshirt size and color
   // TOOD: make sure you annotate the params and return type of the function
-  // type TSize =
-  // type TColor =
-  // type TTshirt =
-  // function createTshirt(size, color) {
-  //   return `${size}-${color}`;
-  // }
-  // const tshirt = createTshirt("S", "red");
+  type TSize = 'S' | 'M' | 'L' | 'XL' | 'XXL';
+  type TColor = 'red' | 'green' | 'blue';
+  type TTshirt = `${string}-${string}`;
+  function createTshirt(size: TSize, color: TColor): TTshirt {
+    return `${size}-${color}`;
+  }
+  const tshirt = createTshirt('S', 'red');
+  console.log(tshirt);
 }
 exercise49();
 
@@ -117,9 +118,28 @@ exercise49();
 function exerciseA() {
   // TODO: create a function to determine if two strings are an anagram
   // HINT: A word is an anagram of another if you can rearrange its characters to produce the second word.
+  type DStrings = {
+    [key: string]: number;
+  };
+
   function areAnagrams(s1: string, s2: string): boolean {
-    console.log('areAnagrams', s1, s2);
-    return false;
+    if (s1.length !== s2.length) {
+      return false;
+    }
+    const arr1: string[] = s1.replace(/\s/g, '').toLowerCase().split('');
+    const arr2: string[] = s2.replace(/\s/g, '').toLowerCase().split('');
+
+    const dictionary: DStrings = {};
+    for (const char of arr1) {
+      dictionary[char] = (dictionary[char] || 0) + 1;
+    }
+
+    for (const item of arr2) {
+      if (!dictionary[item]) {
+        return false;
+      }
+    }
+    return true;
   }
 
   console.assert(areAnagrams('listen', 'silent') === true);
