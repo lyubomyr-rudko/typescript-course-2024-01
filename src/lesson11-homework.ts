ab15689e8ee6d4357dfc07b41a893feab3f76394
Valerii Shkliar <walerik2848@gmail.com>
2024-02-19 12:38:08 +0200

    Homework-11

--- src/lesson11-homework.ts
+++ src/lesson11-homework.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Read the following typescript documentation:
 // 1. https://www.typescriptlang.org/docs/handbook/2/objects.html
 // 2. https://www.typescriptlang.org/docs/handbook/2/generics.html
@@ -6,7 +8,7 @@
 // Use infer keyword
 function exercise46() {
   // TODO: remove the next line
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   function createUser(firstName: string, lastName: string, age: number) {
     const id = (Math.random() * 100000).toString();
 
@@ -17,31 +19,35 @@ function exercise46() {
       id,
     };
   }
-
   // TODO: create a type that extracts the type of the first argument of a function FirstParamType
-  // type FirstParameter<T> = ... // use infer keyword
+  type FirstParameter<T> = T extends (firstPar: infer P, ...args: any) => any
+    ? P
+    : never; // use infer keyword
+
   // TODO: uncomment the following line and fix the error
-  // type TCreateUserFirstArg = FirstParameter<typeof createUser>; // string
+  type TCreateUserFirstArg = FirstParameter<typeof createUser>; // string
 
   // TODO: create a type that extracts the return type of a function ReturnType
-  // type MyReturnType<T> = ... // use infer keyword
+  type MyReturnType<T> = T extends (...args: any) => infer R ? R : never; // use infer keyword
   // TODO: uncomment the following line and fix the error
-  // type TCreateUserReturnType = MyReturnType<typeof createUser>; // { firstName: string; lastName: string; age: number; id: string; }
+  type TCreateUserReturnType = MyReturnType<typeof createUser>; // { firstName: string; lastName: string; age: number; id: string; }
 
   // TODO: create a type that is a mixture of prev two functions
   // 1. it extracts the type of the first argument of a function FirstParamType
   // 2. it also extracts the return type of the function ReturnType
   // And creates a new function type definition with the same return type, and the first param only
-  // type FirstParameterFunction<T> = ... // use infer keyword
-  // type TCreateUserFirstArgAndReturnType = FirstParameterFunction<typeof createUser>; // (firstName: string, lastName: string, age: number) => { firstName: string; lastName: string; age: number; id: string; }
+  type FirstParameterFunction<T> = T extends (
+    firstPar: infer TPar,
+    ...args: any
+  ) => infer TReturn
+    ? (onePar: TPar) => TReturn
+    : never; // use infer keyword
+  type TCreateUserFirstArgAndReturnType = FirstParameterFunction<
+    typeof createUser
+  >; // (firstName: string, lastName: string, age: number) => { firstName: string; lastName: string; age: number; id: string; }
 
   // TODO: replace the next line with the correct type definition
-  type TCreateUserFirstArgAndReturnType = (firstName: string) => {
-    firstName: string;
-    lastName: string;
-    age: number;
-    id: string;
-  };
+
   const fn: TCreateUserFirstArgAndReturnType = (firstName: string) => {
     return {
       firstName,
@@ -54,31 +60,32 @@ function exercise46() {
 }
 exercise46();
 
-// Use mappping types
+// Use mapping types
 function exercise47() {
   // implement TRecord<K,T> mapped type that takes two types T and K
   // K must be a union of strings or numbers or symbols
   // the mapped type should create a new type that has all properties included in list K,
   // and the value of each property is T
-  // type TRecord<K extends ..., T> = {
-  //   [... in ...]: ...;
-  // };
+  type TStringOrNumberOrSymbol = string | number | symbol;
+  type TRecord<K extends TStringOrNumberOrSymbol, T> = {
+    [key in K]: T;
+  };
   // TODO: uncomment the following code and check if your mapped type works
-  // type TPoint = TRecord<"x" | "y" | "z", number>;
-  // const point: TPoint = {
-  //   x: 1,
-  //   y: 2,
-  //   z: 3,
-  // };
+  type TPoint = TRecord<'x' | 'y' | 'z', number>;
+  const point: TPoint = {
+    x: 1,
+    y: 2,
+    z: 3,
+  };
 }
 exercise47();
 
-// Use mappping types modifiers
+// Use mapping types modifiers
 function exercise48() {
   // implement mapped type that makes all properties of T optional and nullable
-  // type TPartialNullable<T> = {
-  //   [... in ...]: ...;
-  // };
+  type TPartialNullable<T> = {
+    [key in keyof T]+?: T[key] | null;
+  };
 
   type TPoint = {
     x: number;
@@ -90,9 +97,11 @@ function exercise48() {
   console.log(p0);
 
   // TODO: uncomment the following code and check if your mapped type works
-  // type TNullablePoint = TPartialNullable<TPoint>;
-  // const p1: TNullablePoint = { x: 10 };
-  // const p2: TNullablePoint = { x: 10, y: null };
+  type TNullablePoint = TPartialNullable<TPoint>;
+  const p1: TNullablePoint = { x: 10 };
+  const p2: TNullablePoint = { x: 10, y: null };
+  console.log(p1);
+  console.log(p2);
 }
 exercise48();
 
@@ -103,13 +112,15 @@ function exercise49() {
   // TODO: create a type that represents a string that contains Tshirts sizes and colors (e.g. "S-red", "M-green", "L-blue")
   // TODO: create a function that takes a size and a color and returns a Tshirt size and color
   // TOOD: make sure you annotate the params and return type of the function
-  // type TSize =
-  // type TColor =
-  // type TTshirt =
-  // function createTshirt(size, color) {
-  //   return `${size}-${color}`;
-  // }
-  // const tshirt = createTshirt("S", "red");
+  type TSize = 'S' | 'M' | 'L' | 'XL' | 'XXL';
+  type TColor = 'red' | 'green' | 'blue';
+  type TTshirt = (size: TSize, color: TColor) => `${TSize}-${TColor}`;
+
+  const createTshirt: TTshirt = function (size, color) {
+    return `${size}-${color}`;
+  };
+  const tshirt = createTshirt('S', 'red');
+  console.log(tshirt);
 }
 exercise49();
 
@@ -119,7 +130,20 @@ function exerciseA() {
   // HINT: A word is an anagram of another if you can rearrange its characters to produce the second word.
   function areAnagrams(s1: string, s2: string): boolean {
     console.log('areAnagrams', s1, s2);
+
+    if (s1.length === s2.length) {
+      const charactersOfS1 = s1.toLowerCase().split('').sort().join();
+      const charactersOfS2 = s2.toLowerCase().split('').sort().join();
+
+      return compareFn(charactersOfS1, charactersOfS2);
+    }
     return false;
+  }
+  function compareFn(s1: string, s2: string) {
+    if (s1.length === 0) return true;
+    if (s1[0] !== s2[0]) return false;
+
+    return compareFn(s1.slice(1), s2.slice(1));
   }
 
   console.assert(areAnagrams('listen', 'silent') === true);
@@ -129,7 +153,3 @@ function exerciseA() {
   // HINT: consider exercise33 for code reuse ideas
 }
 exerciseA();
-
-export default {
-  test: 'test',
-};
