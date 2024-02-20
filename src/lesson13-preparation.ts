9235371ab30ba684469d55ff4acc333d9bfdd5fb
Lyubomyr Rudko <lrudko@shutterstock.com>
2024-02-20 13:04:08 +0200

    lesson 12 udpate

--- src/lesson13-preparation.ts
+++ src/lesson13-preparation.ts
@@ -4,207 +4,6 @@
 // ********* Lesson 13 *********
 
 function lesson13() {
-  // Utility This type
-  function utilityThisType() {
-    // ThisType is a built-in utility type that allows you to specify the type of this inside an object literal, or inside a class declaration.
-
-    type Math = {
-      double(): void;
-      half(): void;
-      pow(n: number): void;
-    };
-
-    const math: Math = {
-      double(this: { value: number }) {
-        this.value *= 2;
-      },
-      half(this: { value: number }) {
-        this.value *= 0.5;
-      },
-      pow(this: { value: number }, n: number) {
-        this.value **= n;
-      },
-    };
-
-    const obj = {
-      value: 2,
-      ...math,
-    };
-
-    obj.double();
-
-    const math2: Math & ThisType<{ value: number }> = {
-      double() {
-        this.value *= 2;
-      },
-      half() {
-        this.value *= 0.5;
-      },
-      pow(n: number) {
-        this.value **= n;
-      },
-    };
-
-    const obj2 = {
-      value: 2,
-      ...math2,
-    };
-
-    obj2.double();
-
-    // example 2
-    type TStateDescriptin<D, M> = {
-      data: D;
-      methods: M & ThisType<D & M>;
-    };
-
-    function createState<D, M>(desc: TStateDescriptin<D, M>): D & M {
-      return {
-        ...desc.data,
-        ...desc.methods,
-      };
-    }
-
-    const state = createState({
-      data: {
-        name: 'John',
-        age: 20,
-      },
-      methods: {
-        getBirthYear() {
-          return new Date().getFullYear() - this.age;
-        },
-      },
-    });
-
-    console.log('birthYear ', state.getBirthYear());
-    console.log('name ', state.age);
-    console.log('name ', state.name);
-  }
-  utilityThisType();
-
-  // String Manipulation Utilities
-  function stringManipulationUtilities() {
-    // Uppercase
-    // Lowercase
-    // Capitalize
-    // Uncapitalize
-
-    /**
-     * Convert string literal type to uppercase
-     */
-    type Uppercase<S extends string> = intrinsic;
-
-    /**
-     * Convert string literal type to lowercase
-     */
-    type Lowercase<S extends string> = intrinsic;
-
-    /**
-     * Convert first character of string literal type to uppercase
-     */
-    type Capitalize<S extends string> = intrinsic;
-
-    /**
-     * Convert first character of string literal type to lowercase
-     */
-    type Uncapitalize<S extends string> = intrinsic;
-
-    // examples
-    type Sizes = 'small' | 'medium' | 'large';
-    type UpperCaseSizes = Uppercase<Sizes>;
-    type LowerCaseSizes = Lowercase<UpperCaseSizes>;
-
-    type Colors = 'red' | 'green' | 'blue';
-    type WidgetTypes = `${Uppercase<Sizes>}-${Colors}`;
-    const a: WidgetTypes = 'SMALL-red';
-
-    // used cases for string manipulation utilities
-    // gettter
-    type Getters<T> = {
-      [K in keyof T & string as `get${Capitalize<K>}`]: () => T[K];
-    };
-    // T & string - will non-string keys
-    // as - will rename keys
-
-    type TPoint = {
-      x: number;
-      y: number;
-      z: number;
-      name: string;
-    };
-    type TPointGetters = Getters<TPoint>;
-    const pointGetter: TPoint & TPointGetters = {
-      x: 1,
-      y: 2,
-      z: 3,
-      name: 'point',
-      getX() {
-        return this.x;
-      },
-      getY() {
-        return this.y;
-      },
-      getZ() {
-        return this.z;
-      },
-      getName() {
-        return this.name;
-      },
-    };
-  }
-  stringManipulationUtilities();
-
-  // Awaited<T> Utility
-  async function awaitedUtilityType() {
-    const first: Promise<string> = new Promise<string>((resolve, reject) =>
-      resolve('Superman'),
-    );
-    const second: Promise<Promise<string>> = new Promise<Promise<string>>(
-      (resolve, reject) =>
-        resolve(new Promise<string>((resolve, reject) => resolve('Batman'))),
-    );
-    const third: Promise<Promise<Promise<string>>> = new Promise<
-      Promise<Promise<string>>
-    >((resolve, reject) =>
-      resolve(
-        new Promise<Promise<string>>((resolve, reject) =>
-          resolve(
-            new Promise<string>((resolve, reject) => resolve('Spiderman')),
-          ),
-        ),
-      ),
-    );
-
-    const firstResult = await first;
-    // const secondResult = await (await second);
-    // const thirdResult = await (await (await third));
-    const secondResult = await second;
-    const thirdResult = await third;
-
-    type FirstAwaited = Awaited<typeof first>;
-    type SecondAwaited = Awaited<typeof second>;
-    type ThirdAwaited = Awaited<typeof third>;
-
-    // Awaited type is a built-in utility type designed to get the type of a promise result, unwrapping nested promises
-
-    /**
-     * Recursively unwraps the "awaited type" of a type. Non-promise "thenables" should resolve to `never`. This emulates the behavior of `await`.
-     */
-    type Awaited<T> = T extends null | undefined
-      ? T // special case for `null | undefined` when not in `--strictNullChecks` mode
-      : // if it is an thenable object (is object and has a callable `then` property)
-        // then- extracts the type of the value that the thenable resolves to (resolve is first argument of then)
-        T extends object & { then(onfulfilled: infer F, ...args: infer _): any } // `await` only unwraps object types with a callable `then`. Non-object types are not unwrapped
-        ? F extends (value: infer V, ...args: infer _) => any // if the argument to `then` is callable, extracts the first argument
-          ? Awaited<V> // recursively unwrap the value
-          : never // the argument to `then` was not callable
-        : T; // non-object or non-thenable
-
-    // third.then((thirdResult) => console.log(thirdResult));
-  }
-  awaitedUtilityType();
-
   // Enums
   function enums() {
     enum FileAccess {
@@ -233,5 +32,15 @@ function lesson13() {
     checkAccess(5);
   }
   enums();
+
+  // React with Typescript - more on hooks - performance optimization
+  // - useCallback
+  // - useMemo
+  // - useRef
+  // - useImperativeHandle
+  // - useReducer
+  // - useContext
+  // vite: https://vitejs.dev/guide/#scaffolding-your-first-vite-project
+  // $ npm create vite@latest
 }
 lesson13();
