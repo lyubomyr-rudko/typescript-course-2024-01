b29f305e7a81dc4d2f834f074b9deceeb7cb6e1d
Daniil Fomenko <danil.fomchik@gmail.com>
2024-03-02 00:18:38 +0200

    finish hw14

--- src/lesson14-homework.test.ts
+++ src/lesson14-homework.test.ts
@@ -2,20 +2,55 @@ import { describe, it, expect } from '@jest/globals';
 
 import { excerciseA, excerciseB, excerciseC } from './lesson14-homework';
 
-describe('excerciseA', () => {
+const { cqList } = excerciseA();
+const { countElements } = excerciseB();
+const { productArrayConstructor } = excerciseC();
+
+describe('cqList', () => {
   it('should be defined', () => {
-    expect(excerciseA).toBeDefined();
+    expect(cqList).toBeDefined();
+  });
+
+  it('should return correct result', () => {
+    expect(cqList([6, 2, 1, 8, 10])).toBe(16);
+    expect(cqList([1, 1, 11, 2, 3])).toBe(6);
+  });
+
+  it('should return 0', () => {
+    expect(cqList(null)).toBe(0);
+    expect(cqList(undefined)).toBe(0);
+    expect(cqList([])).toBe(0);
+    expect(cqList([12])).toBe(0);
   });
 });
 
-describe('excerciseB', () => {
+describe('countElements', () => {
   it('should be defined', () => {
-    expect(excerciseB).toBeDefined();
+    expect(countElements).toBeDefined();
+  });
+
+  it('should return correct result', () => {
+    expect(
+      countElements([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),
+    ).toEqual([10, -65]);
+  });
+
+  it('should return an empty array', () => {
+    expect(countElements(null)).toEqual([]);
+    expect(countElements([])).toEqual([]);
   });
 });
 
-describe('excerciseC', () => {
+describe('productArrayConstructor', () => {
   it('should be defined', () => {
-    expect(excerciseC).toBeDefined();
+    expect(productArrayConstructor).toBeDefined();
+  });
+
+  it('should return correct result', () => {
+    expect(productArrayConstructor([10, 20])).toEqual([20, 10]);
+    expect(productArrayConstructor([1, 2, 3, 4])).toEqual([24, 12, 8, 6]);
+    expect(productArrayConstructor([10, 3, 5, 6, 2])).toEqual([
+      180, 600, 360, 300, 900,
+    ]);
   });
 });
--- src/lesson14-homework.ts
+++ src/lesson14-homework.ts
@@ -10,6 +10,32 @@ export function excerciseA() {
   // { 1, 1, 11, 2, 3 } => 6
   // Input validation
   // If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+  function getSum(elements: number[]): number {
+    return elements.reduce((acum, item) => acum + item);
+  }
+
+  function getExcludedElements(elements: number[]): number[] {
+    const minValueIndex = elements.indexOf(Math.min(...elements));
+    const maxValueIndex = elements.indexOf(Math.max(...elements));
+
+    return [maxValueIndex, minValueIndex];
+  }
+
+  function cqList(elements?: number[] | null): number {
+    if (elements && elements.length > 1) {
+      const excludedElements = getExcludedElements(elements);
+
+      excludedElements.forEach((index) => {
+        elements.splice(index, 1);
+      });
+
+      return getSum(elements);
+    }
+
+    return 0;
+  }
+
+  return { cqList };
 }
 excerciseA();
 
@@ -19,6 +45,30 @@ export function excerciseB() {
   // If the input is an empty array or is null, return an empty array.
   // Example
   // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+  function countElements(array: number[] | null): number[] {
+    if (array && array.length > 0) {
+      let positiveNumbersCount = 0;
+      let negativeNumbersSum = 0;
+
+      array.forEach((item) => {
+        if (item > 0) {
+          positiveNumbersCount++;
+        } else {
+          negativeNumbersSum += item;
+        }
+      });
+
+      return [positiveNumbersCount, negativeNumbersSum];
+    }
+
+    return [];
+  }
+
+  console.log(
+    countElements([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),
+  );
+
+  return { countElements };
 }
 excerciseB();
 
@@ -29,10 +79,25 @@ export function excerciseC() {
   // Array/list's numbers Will be only Positives
   // Repetition of numbers in the array/list could occur.
   // [10,20] return ==> [20,10]
+
   // [1,2,3,4] return ==> [24,12,8,6]
+
   // [1,5,2] return ==> [10,2,5]
+
   // [10,3,5,6,2] return ==> [180,600,360,300,900]
+  // 1800;
+
   // implement the funciton avoiding nested loops, with O(N)
+  function productArrayConstructor(array: number[]): number[] {
+    const arrayMultiplication = array.reduce((acum, item) => acum * item);
+    const result = array.map((item) => arrayMultiplication / item);
+
+    return result;
+  }
+
+  console.log(productArrayConstructor([1, 2, 3, 4]));
+
+  return { productArrayConstructor };
 }
 excerciseC();
 
@@ -43,7 +108,68 @@ function excerciseD() {
   // Tuesday - Single responsibility principle
   // Wednesday - Open-Closed Principle
   // Thursday - Liskov Substitution Principle
-  // Friday - Interface Segregation Principle
+
+  // ++Friday - Interface Segregation Principle
+  // цей принцип заключається у розділенні інтерфейсу на менші та більш специфічні інтерфейси, ніж один великий, в якому буде намішане усе підряд
+
+  // Приклад:
+  // without Interface Segregation Principle
+  interface IIncorrectUser {
+    name: string;
+    domain: string;
+    age: number;
+    gender: string;
+    university: string;
+  }
+
+  const incorrectUser: IIncorrectUser = {
+    name: 'Sarah',
+    domain: 'none',
+    age: 11,
+    gender: 'female',
+    university: 'none',
+  };
+
+  console.log('incorrectUser-->', incorrectUser);
+
+  // with Interface Segregation Principle
+  interface IUserWithDomain {
+    domain: string;
+  }
+
+  interface IUserWithUniversity {
+    university: string;
+  }
+
+  interface IUser1 extends IUserWithDomain {
+    name: string;
+    age: number;
+    gender: string;
+  }
+
+  interface IUser2 extends IUserWithUniversity {
+    name: string;
+    age: number;
+    gender: string;
+  }
+
+  const user1: IUser1 = {
+    name: 'Alex',
+    age: 22,
+    gender: 'male',
+    domain: 'a.domain',
+  };
+
+  const user2: IUser2 = {
+    name: 'John',
+    age: 18,
+    gender: 'male',
+    university: 'university',
+  };
+
+  console.log('user1-->', user1);
+  console.log('user2-->', user2);
+
   // Saturday - Dependency Inversion Principle
   //
   // Plan in advance, so your homework day matches the principle you want to use as an example.
