42d2f83ff4190fa00c984568275cfdbd1c465d9c
Oleksii Fadieiev <ofadieiev@macpaw.com>
2024-03-07 18:23:51 +0100

    HW-15

--- src/lesson15-homework.ts
+++ src/lesson15-homework.ts
@@ -1,21 +1,34 @@
 export {};
 
-// Read all articlas from https://refactoring.guru/uk/design-patterns/creational-patterns
+// Read all articles from https://refactoring.guru/uk/design-patterns/creational-patterns
 // Answer the following questions:
 // 1. Which creational pattern to use when we have family of different kind of objects that created according to some condition, for example - different databases maybe supported: Oracle, SQLServer, H2
+// AbstractFactory
 // 2. Which creational pattern to use when we want to create only one instance of a class?
-// 3. Which creational pattern to use when creation of the object is very time consuming, and we want to be able to duplicate the object?
+// Singleton
+// 3. Which creational pattern to use when creation of the object is very time-consuming, and we want to be able to duplicate the object?
+// Prototype
 // 4. Which creational pattern to use when we need to pass a lot of parameters to the constructor and some of these parameters are completely irrelevant for the specific configuration of the object?
+// Builder
 
-function excerciseA() {
+function exerciseA() {
   // Observe this implementation class ConfigurationManager, which is responsible for reading and writing configuration data.
   class ConfigManager {
+    private static instance: ConfigManager;
     private config: {
       [key: string]: string | number;
     };
 
-    constructor() {
+    private constructor() {
       this.config = {};
+    }
+
+    public static getInstance(): ConfigManager {
+      if (!ConfigManager.instance) {
+        ConfigManager.instance = new ConfigManager();
+      }
+
+      return ConfigManager.instance;
     }
 
     // Method to get a config parameter
@@ -33,8 +46,8 @@ function excerciseA() {
   // Rewrite the class to use the standard implementation of that pattern.
   //
   // Update the way of creating the instance of the class ConfigurationManager
-  const configManager = new ConfigManager();
+  const configManager = ConfigManager.getInstance();
   configManager.setConfig('name', 'John');
   console.log(configManager.getConfig('name'));
 }
-excerciseA();
+exerciseA();
