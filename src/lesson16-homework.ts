bcd102b8359163b8a60440eb8889f20d99723e91
vasyl <makintow@gmail.com>
2024-03-10 17:48:01 +0200

    Vasyl TS HW lesson-16

--- src/lesson16-homework.ts
+++ src/lesson16-homework.ts
@@ -3,16 +3,25 @@ export {};
 // Read all articlas from https://refactoring.guru/uk/design-patterns/structural-patterns
 // From the list of the structural patterns: Composite, Decorator, Bridge, Adapter, Facade, Proxy select:
 // 1. Which  structural pattern to use when we want to convert one interface to another?
+// Adapter
 // 2. Which  structural pattern to use when we want to represent a complex object as a tree-like structure?
+// Composite
 // 3. Which  structural pattern to use when we want to add new functionality to an object without altering its structure, create multiple mixtures of options?
+// Decorator
 // 4. Which  structural pattern to use to create a simplified interface to a complex system?
+// Facade
 // 5. Which  structural pattern allows create object-replacement of original object, that would adding access check or caching before calling original method?
+// Proxy
 
 function excerciseA() {
   // Imagine you have a class for a Computer, and you would want user to be able to add additional functionality to it.
   // Use one of the Structural patterns to implement this requirement.
   // Rewrite the class to use the standard implementation of that pattern.
-  class ComputerBase {
+
+  interface IComputerBase {
+    printConfiguration(): string;
+  }
+  class ComputerBase implements IComputerBase {
     constructor() {}
 
     printConfiguration() {
@@ -20,65 +29,59 @@ function excerciseA() {
     }
   }
 
-  class ComputerWithProcessor extends ComputerBase {
-    printConfiguration() {
-      return 'Computer base, with processor';
+  class BaseDecorator implements IComputerBase {
+    protected component: IComputerBase;
+
+    constructor(component: IComputerBase) {
+      this.component = component;
+    }
+
+    printConfiguration(): string {
+      return this.component.printConfiguration();
     }
   }
 
-  class ComputerWithMemory extends ComputerBase {
+  class ComputerWithProcessor extends BaseDecorator {
     printConfiguration() {
-      return 'Computer base, with memory';
+      return `${super.printConfiguration()}, with processor`;
     }
   }
 
-  class ComputerWithHardDrive extends ComputerBase {
+  class ComputerWithMemory extends BaseDecorator {
     printConfiguration() {
-      return 'Computer base, with hard drive';
+      return `${super.printConfiguration()}, with memory`;
     }
   }
 
-  class ComputerWithProcessorAndMemory extends ComputerBase {
+  class ComputerWithHardDrive extends BaseDecorator {
     printConfiguration() {
-      return 'Computer base, with processor and memory';
-    }
-  }
-
-  class ComputerWithProcessorAndHardDrive extends ComputerBase {
-    printConfiguration() {
-      return 'Computer base, with processor and hard drive';
-    }
-  }
-
-  class ComputerWithMemoryAndHardDrive extends ComputerBase {
-    printConfiguration() {
-      return 'Computer base, with memory and hard drive';
-    }
-  }
-
-  class ComputerWithProcessorAndMemoryAndHardDrive extends ComputerBase {
-    printConfiguration() {
-      return 'Computer base, with processor, memory and hard drive';
+      return `${super.printConfiguration()}, with hard drive`;
     }
   }
 
   const computer = new ComputerBase();
   console.log(computer.printConfiguration());
-  const computerWithProcessor = new ComputerWithProcessor();
+  const computerWithProcessor = new ComputerWithProcessor(computer);
   console.log(computerWithProcessor.printConfiguration());
-  const computerWithMemory = new ComputerWithMemory();
+  const computerWithMemory = new ComputerWithMemory(computer);
   console.log(computerWithMemory.printConfiguration());
-  const computerWithHardDrive = new ComputerWithHardDrive();
+  const computerWithHardDrive = new ComputerWithHardDrive(computer);
   console.log(computerWithHardDrive.printConfiguration());
-  const computerWithProcessorAndMemory = new ComputerWithProcessorAndMemory();
+  const computerWithProcessorAndMemory = new ComputerWithMemory(
+    computerWithProcessor,
+  );
   console.log(computerWithProcessorAndMemory.printConfiguration());
-  const computerWithProcessorAndHardDrive =
-    new ComputerWithProcessorAndHardDrive();
+  const computerWithProcessorAndHardDrive = new ComputerWithHardDrive(
+    computerWithProcessor,
+  );
   console.log(computerWithProcessorAndHardDrive.printConfiguration());
-  const computerWithMemoryAndHardDrive = new ComputerWithMemoryAndHardDrive();
+  const computerWithMemoryAndHardDrive = new ComputerWithHardDrive(
+    computerWithMemory,
+  );
   console.log(computerWithMemoryAndHardDrive.printConfiguration());
-  const computerWithProcessorAndMemoryAndHardDrive =
-    new ComputerWithProcessorAndMemoryAndHardDrive();
+  const computerWithProcessorAndMemoryAndHardDrive = new ComputerWithHardDrive(
+    computerWithProcessorAndMemory,
+  );
   console.log(computerWithProcessorAndMemoryAndHardDrive.printConfiguration());
 }
 excerciseA();
