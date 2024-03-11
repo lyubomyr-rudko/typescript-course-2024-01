aa010fe1159071d22d51a6f92d8b48f735bf5a8b
Artem-Kelioglo <76010430+Artem-Kelioglo@users.noreply.github.com>
2024-03-11 12:49:22 +0200

    2 version

--- src/lesson16-homework.ts
+++ src/lesson16-homework.ts
@@ -17,73 +17,90 @@ function excerciseA() {
   // Imagine you have a class for a Computer, and you would want user to be able to add additional functionality to it.
   // Use one of the Structural patterns to implement this requirement.
   // Rewrite the class to use the standard implementation of that pattern.
-  class ComputerBase {
-    constructor() {}
 
+  interface Computer {
+    printConfiguration(): string;
+  }
+
+  class BaseComputer implements Computer {
     printConfiguration() {
       return 'Computer base';
     }
   }
 
-  class ComputerWithProcessor extends ComputerBase {
-    printConfiguration() {
-      return 'Computer base, with processor';
+  interface AdditionalFunctionality {
+    addFunctionality(): string;
+  }
+
+  class Processor implements AdditionalFunctionality {
+    addFunctionality() {
+      return 'with processor';
     }
   }
 
-  class ComputerWithMemory extends ComputerBase {
-    printConfiguration() {
-      return 'Computer base, with memory';
+  class Memory implements AdditionalFunctionality {
+    addFunctionality() {
+      return 'with memory';
     }
   }
 
-  class ComputerWithHardDrive extends ComputerBase {
-    printConfiguration() {
-      return 'Computer base, with hard drive';
+  class HardDrive implements AdditionalFunctionality {
+    addFunctionality() {
+      return 'with hard drive';
     }
   }
 
-  class ComputerWithProcessorAndMemory extends ComputerBase {
+  class ComputerAdapter implements Computer {
+    constructor(
+      private computer: Computer,
+      private functionalities: AdditionalFunctionality[],
+    ) {}
+
     printConfiguration() {
-      return 'Computer base, with processor and memory';
+      const baseConfiguration = this.computer.printConfiguration();
+      const addedFunctionalities = this.functionalities
+        .map((func) => func.addFunctionality())
+        .join(', ');
+      return `${baseConfiguration}, ${addedFunctionalities}`;
     }
   }
 
-  class ComputerWithProcessorAndHardDrive extends ComputerBase {
-    printConfiguration() {
-      return 'Computer base, with processor and hard drive';
-    }
-  }
-
-  class ComputerWithMemoryAndHardDrive extends ComputerBase {
-    printConfiguration() {
-      return 'Computer base, with memory and hard drive';
-    }
-  }
-
-  class ComputerWithProcessorAndMemoryAndHardDrive extends ComputerBase {
-    printConfiguration() {
-      return 'Computer base, with processor, memory and hard drive';
-    }
-  }
-
-  const computer = new ComputerBase();
+  const computer: Computer = new BaseComputer();
   console.log(computer.printConfiguration());
-  const computerWithProcessor = new ComputerWithProcessor();
+
+  const computerWithProcessor = new ComputerAdapter(computer, [
+    new Processor(),
+  ]);
   console.log(computerWithProcessor.printConfiguration());
-  const computerWithMemory = new ComputerWithMemory();
+
+  const computerWithMemory = new ComputerAdapter(computer, [new Memory()]);
   console.log(computerWithMemory.printConfiguration());
-  const computerWithHardDrive = new ComputerWithHardDrive();
+
+  const computerWithHardDrive = new ComputerAdapter(computer, [
+    new HardDrive(),
+  ]);
   console.log(computerWithHardDrive.printConfiguration());
-  const computerWithProcessorAndMemory = new ComputerWithProcessorAndMemory();
+
+  const computerWithProcessorAndMemory = new ComputerAdapter(computer, [
+    new Processor(),
+    new Memory(),
+  ]);
   console.log(computerWithProcessorAndMemory.printConfiguration());
-  const computerWithProcessorAndHardDrive =
-    new ComputerWithProcessorAndHardDrive();
+  const computerWithProcessorAndHardDrive = new ComputerAdapter(computer, [
+    new Processor(),
+    new HardDrive(),
+  ]);
   console.log(computerWithProcessorAndHardDrive.printConfiguration());
-  const computerWithMemoryAndHardDrive = new ComputerWithMemoryAndHardDrive();
+  const computerWithMemoryAndHardDrive = new ComputerAdapter(computer, [
+    new Memory(),
+    new HardDrive(),
+  ]);
   console.log(computerWithMemoryAndHardDrive.printConfiguration());
-  const computerWithProcessorAndMemoryAndHardDrive =
-    new ComputerWithProcessorAndMemoryAndHardDrive();
+
+  const computerWithProcessorAndMemoryAndHardDrive = new ComputerAdapter(
+    computer,
+    [new Memory(), new HardDrive(), new Processor()],
+  );
   console.log(computerWithProcessorAndMemoryAndHardDrive.printConfiguration());
 }
 excerciseA();
