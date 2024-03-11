e73ca2554b49c5543c3bf20b84d47a42b6610181
Artem-Kelioglo <76010430+Artem-Kelioglo@users.noreply.github.com>
2024-03-11 11:47:34 +0200

    1 version

--- src/lesson16-homework.ts
+++ src/lesson16-homework.ts
@@ -3,10 +3,15 @@ export {};
 // Read all articlas from https://refactoring.guru/uk/design-patterns/structural-patterns
 // From the list of the structural patterns: Composite, Decorator, Bridge, Adapter, Facade, Proxy select:
 // 1. Which  structural pattern to use when we want to convert one interface to another?
+//Adapter
 // 2. Which  structural pattern to use when we want to represent a complex object as a tree-like structure?
+//Composite
 // 3. Which  structural pattern to use when we want to add new functionality to an object without altering its structure, create multiple mixtures of options?
+//Decorator
 // 4. Which  structural pattern to use to create a simplified interface to a complex system?
+//Facade
 // 5. Which  structural pattern allows create object-replacement of original object, that would adding access check or caching before calling original method?
+// Proxy
 
 function excerciseA() {
   // Imagine you have a class for a Computer, and you would want user to be able to add additional functionality to it.
